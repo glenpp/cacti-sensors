02d2ffec317adfbfd87211582643d2356c6f3148
Glen Pit-Pladdy <glenpp@users.noreply.github.com>
2023-07-01 13:36:48 +0100

    tidy up, small fixes

--- sensors_generic.py
+++ sensors_generic.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
-# Copyright (C) 2021  Glen Pitt-Pladdy
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-#
-#
-# See: https://www.pitt-pladdy.com/blog/_20091031-160232_0000_LM_Sensors_stats_on_Cacti_via_SNMP_/
+"""
+Copyright (C) 2021-2023  Glen Pitt-Pladdy
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+
+See: https://github.com/glenpp/cacti-sensors
+
+Version: 20230701
+"""
 
 import sys
 import subprocess
@@ -26,12 +30,16 @@ import re
 
 
 # absolute path is safer
-SENSORS = '/usr/bin/sensors'
+SENSORS_COMMAND = [
+    '/usr/bin/sensors',
+    '-j',
+]
 
 
 def usage():
-    print("Usage: {} <temps|voltages|fans> <index|labels|readings|min|max|crit>".format(sys.argv[0]))
+    print(f"Usage: {sys.argv[0]} <temps|voltages|fans> <index|labels|value|min|max|crit>", file=sys.stderr)
     sys.exit(1)
+
 
 def main():
     try:
@@ -39,14 +47,14 @@ def main():
         output_mode = sys.argv[2]
     except IndexError:
         usage()
-    # run in json mode
+    # capture sensors data
     proc = subprocess.run(
-        [SENSORS, '-j'],
+        SENSORS_COMMAND,
         capture_output=True,
         check=True,
     )
     sensors = json.loads(proc.stdout)
-    # pick out by 
+    # pick out by sensor
     readings = {}
     for chip, chip_sensors in sensors.items():
         adapter = chip_sensors.get('Adapter')
@@ -59,20 +67,19 @@ def main():
             elif sensor_type == 'fans':
                 prefix_pattern = r'^fan(\d+)_'
             else:
-                raise ValueError("BUG! Unhanled sensor type: {}".format(sensor_type))
+                raise ValueError(f"BUG! Unhanled sensor type: {sensor_type}")
             index = None
             prefix = None
-            for metric, value in data.items():
+            for metric in data.keys():
                 match = re.fullmatch(prefix_pattern + 'input$', metric)
                 if not match:
                     continue
                 # matching item - generate index
-                index = '{}.{}.{:02d}'.format(chip, sensor_type, int(match.group(1)))
+                index = f'{chip}.{sensor_type}.{int(match.group(1)):02d}'
                 prefix = metric.rsplit('_', 1)[0]
                 break
             if not index:
                 continue
-            # 
             readings[index] = {
                 'chip': chip,
                 'adapter': adapter,
@@ -91,9 +98,9 @@ def main():
         elif output_mode == 'labels':
             print(readings[index]['label'])
         elif output_mode in readings[index]['metrics']:
-                print(readings[index]['metrics'][output_mode])
+            print(readings[index]['metrics'][output_mode])
         else:
-            raise ValueError("BUG! Unhanled mode: {}".format(sensor_mode))
+            raise ValueError(f"BUG! Unhanled mode: {output_mode}")
 
 
 if __name__ == '__main__':
